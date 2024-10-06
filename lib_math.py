47fcde5fad3db2fd957361e29303c61defeedf92
julian-marques <juliandronefpv@gmail.com>
2024-10-06 17:53:42 +0200

    moteur 3D deplacement / rotation / affichage 3D de triangle

--- lib_math.py
+++ lib_math.py
@@ -1,38 +1,87 @@
-import math 
-
-
+import moteur_graphique as mg
+import math
 class Vec2:
     def __init__(self,x,y) -> None:
         self.x = x
         self.y = y
-    def rotate(self, angle, center):
-        cos_theta = math.cos(angle)
-        sin_theta = math.sin(angle)
 
-        x_translated = self.x - center.x
-        y_translated = self.y - center.y
-
-        x_rotated = x_translated * cos_theta - y_translated * sin_theta
-        y_rotated = x_translated * sin_theta + y_translated * cos_theta
-
-        self.x = x_rotated + center.x
-        self.y = y_rotated + center.y
-
-
-
-class Triangle:
-    def __init__(self,p1,p2,p3) -> None:
-        self.p1 = p1
-        self.p2 = p2
-        self.p3 = p3
+    def __mul__(self,c):
+        return Vec2(self.x*c,self.y*c)
     
-    def center(self):
-        cx = (self.p1.x + self.p2.x + self.p3.x) / 3
-        cy = (self.p1.y + self.p2.y + self.p3.y) / 3
-        return Vec2(cx, cy)
+    def __truediv__(self,c):
+        return Vec2(self.x/c,self.y/c)
+    
+    def __add__(self,v):
+        return Vec2(self.x+v.x,self.y+v.y)
+    
+    __radd__ = __add__
+    __rmul__ = __mul__
 
-    def rotate(self, angle):
-        center = self.center()
-        self.p1.rotate(angle, center)
-        self.p2.rotate(angle, center)
-        self.p3.rotate(angle, center)
+    def toScreen(self):
+        return Vec2(((29/13)*mg.height/mg.width*self.x+1)*mg.width/2,
+                    (-self.y+1)*mg.height/2)
+    
+class Vec3:
+    def __init__(self,x,y,z) -> None:
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def __mul__(self,c):
+        return Vec3(self.x*c,self.y*c,self.z*c)
+    
+    def __truediv__(self,c):
+        return Vec3(self.x/c,self.y/c,self.z/c)
+    
+    def __add__(self,v):
+        return Vec3(self.x+v.x,self.y+v.y,self.z+v.z)
+    
+    __radd__ = __add__
+    __rmul__ = __mul__
+
+    def projection(self):
+        return Vec2(self.x, self.y) / self.z
+
+    def rotationX(self,pitch):
+        y1= math.cos(pitch) * self.y - math.sin(pitch) * self.z
+        z1= math.sin(pitch) * self.y + math.cos(pitch) * self.z
+        return Vec3(self.x, y1, z1)
+
+    def rotationY(self,yaw):
+        x1=  math.cos(yaw) * self.x - math.sin(yaw) * self.z
+        z1= -math.sin(yaw) * self.x + math.cos(yaw) * self.z
+        return Vec3(x1, self.y, z1)
+
+class Triangle2D:
+    def __init__(self,v1,v2,v3) -> None:
+        self.v1 = v1
+        self.v2 = v2
+        self.v3 = v3
+    
+    def toScreen(self):
+        return Triangle2D(
+            self.v1.toScreen(),
+            self.v2.toScreen(),
+            self.v3.toScreen()
+        )
+        
+
+class Triangle3D:
+    def __init__(self,v1,v2,v3) -> None:
+        self.v1 = v1
+        self.v2 = v2
+        self.v3 = v3
+
+    def projection(self):
+        return Triangle2D(self.v1.projection(),self.v2.projection(),self.v3.projection())
+    
+    def translate(self, v:Vec3):
+        return Triangle3D(self.v1+v,self.v2+v,self.v3+v)
+
+    def rotationX(self, pitch):
+        return Triangle3D(self.v1.rotationX(pitch),self.v2.rotationX(pitch),self.v3.rotationX(pitch))
+
+    def rotationY(self, yaw):
+        return Triangle3D(self.v1.rotationY(yaw),self.v2.rotationY(yaw),self.v3.rotationY(yaw))
+
+
