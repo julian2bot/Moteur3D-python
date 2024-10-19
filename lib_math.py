2a583a07971b50ec02ed07252aaaec6c9e2988a8
julian-marques <juliandronefpv@gmail.com>
2024-10-19 18:15:59 +0200

    correction des mouvements et ajout des annotators

--- lib_math.py
+++ lib_math.py
@@ -1,6 +1,7 @@
 import moteur_graphique as mg
 import math
 
+from typing import Self
 
 class Vec2:
 
@@ -8,19 +9,19 @@ class Vec2:
         self.x = x
         self.y = y
 
-    def __mul__(self, c: int):
+    def __mul__(self, c: int) -> 'Vec2':
         return Vec2(self.x * c, self.y * c)
 
-    def __truediv__(self, c: int):
+    def __truediv__(self, c: int) -> 'Vec2':
         return Vec2(self.x / c, self.y / c)
 
-    def __add__(self, v):
+    def __add__(self, v) -> 'Vec2':
         return Vec2(self.x + v.x, self.y + v.y)
 
     __radd__ = __add__
     __rmul__ = __mul__
 
-    def toScreen(self):
+    def toScreen(self) -> 'Vec2':
         return Vec2(
             ((29 / 13) * mg.height / mg.width * self.x + 1) * mg.width / 2,
             (-self.y + 1) * mg.height / 2)
@@ -33,27 +34,27 @@ class Vec3:
         self.y = y
         self.z = z
 
-    def __mul__(self, c: int):
+    def __mul__(self, c: int) -> 'Vec3':
         return Vec3(self.x * c, self.y * c, self.z * c)
 
-    def __truediv__(self, c: int):
+    def __truediv__(self, c: int) -> 'Vec3':
         return Vec3(self.x / c, self.y / c, self.z / c)
 
-    def __add__(self, v):
+    def __add__(self, v) -> 'Vec3':
         return Vec3(self.x + v.x, self.y + v.y, self.z + v.z)
 
     __radd__ = __add__
     __rmul__ = __mul__
 
-    def projection(self, focalLenth: int):
+    def projection(self, focalLenth: int) -> 'Vec2':
         return focalLenth * Vec2(self.x, self.y) / self.z
 
-    def rotationX(self, pitch: int):
+    def rotationX(self, pitch: int) -> 'Vec3':
         y1 = math.cos(pitch) * self.y - math.sin(pitch) * self.z
         z1 = math.sin(pitch) * self.y + math.cos(pitch) * self.z
         return Vec3(self.x, y1, z1)
 
-    def rotationY(self, yaw: int):
+    def rotationY(self, yaw: int) -> 'Vec3':
         x1 = math.cos(yaw) * self.x - math.sin(yaw) * self.z
         z1 = -math.sin(yaw) * self.x + math.cos(yaw) * self.z
         return Vec3(x1, self.y, z1)
@@ -66,7 +67,7 @@ class Triangle2D:
         self.v2 = v2
         self.v3 = v3
 
-    def toScreen(self):
+    def toScreen(self) -> 'Triangle2D':
         return Triangle2D(self.v1.toScreen(), self.v2.toScreen(),
                           self.v3.toScreen())
 
@@ -83,13 +84,13 @@ class Triangle3D:
                           self.v2.projection(focalLenth),
                           self.v3.projection(focalLenth))
 
-    def translate(self, v: Vec3):
+    def translate(self, v: Vec3) -> 'Triangle3D':
         return Triangle3D(self.v1 + v, self.v2 + v, self.v3 + v)
 
-    def rotationX(self, pitch: int):
+    def rotationX(self, pitch: int) -> 'Triangle3D':
         return Triangle3D(self.v1.rotationX(pitch), self.v2.rotationX(pitch),
                           self.v3.rotationX(pitch))
 
-    def rotationY(self, yaw: int):
+    def rotationY(self, yaw: int) -> 'Triangle3D' :
         return Triangle3D(self.v1.rotationY(yaw), self.v2.rotationY(yaw),
                           self.v3.rotationY(yaw))
--- main.py
+++ main.py
@@ -6,37 +6,14 @@ import time
 
 from pynput.mouse import Listener
 
-import meshio
-
-mesh = meshio.read("rectangle.stl")  # or .off, .vtk, .ply, ...
-# mesh.points, mesh.cells, ...
-
 
 # tri = Triangle3D(Vec3(-0.5, -0.5, 1.5), Vec3(0, 0.5, 1.5),
 #                  Vec3(0.5, -0.5, 1.5))
 
 carre = [
     Triangle3D(Vec3(-0.5, -0.5, 1), Vec3(-0.5, 0.5, 1), Vec3(0.5, 0.5, 1)),
-    Triangle3D(Vec3(-0.5, -0.5, 2), Vec3(0.5, 0.5, 1), Vec3(0.5, -0.5, 1))
+    Triangle3D(Vec3(-0.5, -0.5, 2), Vec3(0.5, 0.5, -1), Vec3(0.5, -0.5, 1))
 ]
-
-
-import numpy as np
-points = np.array(mesh.points)
-min_vals = points.min(axis=0)
-max_vals = points.max(axis=0)
-def normalize_point(point, min_vals, max_vals):
-    return 2 * ((point - min_vals) / (max_vals - min_vals)) - 1
-normalized_points = np.array([normalize_point(point, min_vals, max_vals) for point in mesh.points])
-
-forme = []
-for triangle in mesh.cells[0].data:
-
-    p1 = normalized_points[triangle[0]]  
-    p2 = normalized_points[triangle[1]]  
-    p3 = normalized_points[triangle[2]] 
-
-    forme.append(Triangle3D(Vec3(p1[const.X],p1[const.Y],p1[const.Z]), Vec3(p2[const.X],p2[const.Y],p2[const.Z]), Vec3(p3[const.X],p3[const.Y],p3[const.Z])))
 
 
 
@@ -85,6 +62,14 @@ def inputs(dt: float):
     if keyboard.is_pressed("shift"):
         cam.position.y -= const.DEFAULT_DEPLACEMENT * dt
 
+    # if keyboard.is_pressed("a"):
+    #     cam.focalLenth += .1 * dt
+    # if keyboard.is_pressed("e"):
+    #     cam.focalLenth -= .1 * dt
+
+    if keyboard.is_pressed("c"):
+        exit()
+
 
 dernier = 0
 def camMove(prev_mouse_x, prev_mouse_y, mouse_dx, mouse_dy):
@@ -94,21 +79,19 @@ def camMove(prev_mouse_x, prev_mouse_y, mouse_dx, mouse_dy):
     cam.pitch -= delta_y * const.DEFAULT_DEPLACEMENT / 3 * dt    
     prev_mouse_x = mouse_dx
     prev_mouse_y = mouse_dy
-    print(prev_mouse_x, prev_mouse_y, mouse_dx, mouse_dy, delta_x,delta_y)
     return prev_mouse_x, prev_mouse_y 
+
 prev_mouse_x, prev_mouse_y = mouse_dx, mouse_dy
 while True:
-    
-    
+    fps = 40
+    # time.sleep(0.01)
 
     temps_actuelle = time.time()
     dt = (temps_actuelle - dernier) * 100
     dernier = temps_actuelle
-
     mg.clear(const.BACKGROUND)
-
     inputs(dt)
-    prev_mouse_x, prev_mouse_y = camMove(prev_mouse_x, prev_mouse_y, mouse_dx, mouse_dy)
+    # prev_mouse_x, prev_mouse_y = camMove(prev_mouse_x, prev_mouse_y, mouse_dx, mouse_dy)
 
     # mg.putMesh(forme, cam, const.DEFAULT_CHAR)
     mg.putMesh(carre, cam, const.DEFAULT_CHAR)
